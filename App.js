ff027ace3c8f6c00e221fb3b3b98b36ab1b6cc5e
Mateus <mat.dfabri@gmail.com>
2022-03-13 16:27:00 -0300

    Melhorias de cálculo de %, inversão de sinal e novo esquema de cor

--- App.js
+++ App.js
@@ -51,7 +51,53 @@ export default function App() {
         setLastNumber(currentNumber + " = ")
         calculator()
         return
+      
       case '+/-':
+        const splitNumbers = currentNumber.split(' ')
+        const fistNumber = parseFloat(splitNumbers[0])
+        const lastNumber = parseFloat(splitNumbers[2])
+        const operator = splitNumbers[1]
+
+        if (operator !== '+' && operator !== '-' && operator !== 'x' && operator !== '/') {
+          setCurrentNumber((fistNumber * -1).toString())
+          return
+
+        } else {
+          setCurrentNumber((fistNumber + " " + operator + " " + (lastNumber * -1)).toString())
+          return
+        }
+        
+      case '%':
+        setLastNumber(currentNumber + " %" + " =")
+
+        const splitNumbersPorcent = currentNumber.split(' ')
+        const fistNumberPorcent = parseFloat(splitNumbersPorcent[0])
+        const lastNumberPorcent = parseFloat(splitNumbersPorcent[2])
+        const operatorPorcent = splitNumbersPorcent[1]
+
+        if (operatorPorcent == 'x') {
+          setCurrentNumber(((fistNumberPorcent * lastNumberPorcent) / 100).toString())
+          return
+  
+        } else {
+          if (operatorPorcent == '+') {
+            setCurrentNumber((fistNumberPorcent + ((fistNumberPorcent * lastNumberPorcent) / 100)).toString())
+            return
+  
+          } else {
+            if (operatorPorcent == '-') {
+              setCurrentNumber((fistNumberPorcent - ((fistNumberPorcent * lastNumberPorcent) / 100)).toString())
+              return
+
+            } else {
+              if (operatorPorcent == '/') {
+                setCurrentNumber((fistNumberPorcent / ((fistNumberPorcent * lastNumberPorcent) / 100)).toString())
+                return
+              }
+            }
+          }
+        }
+        
         return
     }
 
@@ -74,12 +120,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#1E1240'}]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'white': '#7c7c7c'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -95,10 +141,10 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#1E1240"
   },
   resultText: {
-    color: "#282F38",
+    color: "white",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
@@ -115,7 +161,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: "#3D0075",
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90, 
